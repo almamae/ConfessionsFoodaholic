4c234dde212db2d35852c17e9f2b5f43e9142b26
Alma Mae J. Bernales <maebernales@gmail.com>
2013-03-04 21:22:57 +0800

    comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  before_filter :get_parent, :authorize
+
+  def new
+    @comment =  @parent.comments.build(params[:comment]) 
+  end
+
+  def create
+    @comment = @parent.comments.build(params[:comment])
+
+    if @comment.save
+      redirect_to post_path(Post.find(@comment.post)), :notice => 'Comment saved!'
+    else
+      redirect_to post_path(Post.find(@comment.post)), :notice => 'Comment not saved!'
+    end
+  end
+  
+  protected
+  
+  def get_parent
+    @parent = Post.find_by_id(params[:post_id]) if params[:post_id]
+    @parent = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+  end
+
+end
